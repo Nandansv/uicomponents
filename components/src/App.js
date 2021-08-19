a083806d4679945f0473b88691029f480ecc739f
Nandan  Mac <nandansvamc1993@hotmail.com>
2021-08-19 23:39:19 +0530

    freeSolo

--- components/src/App.js
+++ components/src/App.js
@@ -11,6 +11,7 @@ export const AutoComplete = (props)  => {
   return (
     <div className="App">
       <Autocomplete
+       freeSolo
         multiple
         options={options}
         getOptionLabel={option => option.title}
