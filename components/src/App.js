257363f0ac9da988fed2c6fdc2ff0cab29876bcd
Nandan  Mac <nandansvamc1993@hotmail.com>
2021-08-19 23:28:32 +0530

    Adds Autocomplete

--- components/src/App.js
+++ components/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { TextField } from '@material-ui/core'
+import { Autocomplete  } from '@material-ui/lab'
 
-function App() {
+export const AutoComplete = (props)  => {
+  const {
+    onSelect,
+    onInputChange,
+    options
+  } = props;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Autocomplete
+        multiple
+        options={options}
+        getOptionLabel={option => option.title}
+        onChange={onSelect} // click on the show tags
+        onInputChange={onInputChange} //** on every input change hitting my api**
+        filterSelectedOptions
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            label="Multiple values"
+            placeholder="Favorites"
+            margin="normal"
+            fullWidth
+          />
+        )}
+      />
+
     </div>
   );
 }
 
-export default App;
+
